336450a48cdc9750e23074b0b0b906975daaf6c7
miladmemarian <milad.memarian@gmail.com>
2018-04-11 15:57:35 -0700

    Note saved.#1

---
+++ index.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const bodyParser = require('body-parser')
+
+const app = express()
+const jsonParser = bodyParser.json()
+
+app.get('/notebook', (req, res) => {
+  MongoClient.connect('mongodb://localhost/notebook', (err, client) => {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    }
+    const db = client.db('notebook')
+    const notes = db.collection('notes')
+
+    notes.find().toArray((err, result) => {
+      if (err) {
+        console.error(err)
+      }
+      else {
+        console.log(result)
+      }
+
+      client.close()
+    })
+  })
+})
+
+app.use(jsonParser)
+
+app.post('/notebook', (req, res) => {
+  MongoClient.connect('mongodb://localhost/notebook', (err, client) => {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    }
+    const db = client.db('notebook')
+    const notes = db.collection('notes')
+    notes.insertOne(req.body, (err, result) => {
+      if (err) {
+        console.error(err)
+      }
+      else {
+        console.log(result)
+      }
+      client.close()
+    })
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Listening on 3000!')
+})
